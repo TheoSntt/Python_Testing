0edd59e559192982c603b873c8e8efd2cb67ba2a
Théo <sinatti.theo@gmail.com>
2023-06-19 17:00:26 +0200

    Initial commit. Added if name==main statement to server.py file to ease the running of the app

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 import json
-from flask import Flask,render_template,request,redirect,flash,url_for
+from flask import abort, Flask, render_template, request, redirect, flash, url_for
 
 
 def loadClubs():
@@ -27,7 +27,13 @@ def index():
 @app.route('/showSummary',methods=['POST'])
 def showSummary():
     club = [club for club in clubs if club['email'] == request.form['email']][0]
-    return render_template('welcome.html',club=club,competitions=competitions)
+    # try:
+    #     club = [club for club in clubs if club['email'] == request.form['email']][0]
+    # except IndexError:
+    #     # return render_template('invalid_account.html')
+    #     abort(403)
+    # return render_template('welcome.html',club=club,competitions=competitions)
+
 
 
 @app.route('/book/<competition>/<club>')
@@ -57,3 +63,7 @@ def purchasePlaces():
 @app.route('/logout')
 def logout():
     return redirect(url_for('index'))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
