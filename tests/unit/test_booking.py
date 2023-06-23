2bf0407d79c5015c5a7b9d5d577ac9285e770df5
Théo <sinatti.theo@gmail.com>
2023-06-23 16:36:18 +0200

    Add/Modify tests so they expect clubs not to be able to book more than 12 places. They now fail. Will now add the code so these tests pass.

--- tests/unit/test_booking.py
+++ tests/unit/test_booking.py
@@ -17,23 +17,32 @@ class TestBookingClass:
         data = response.data.decode()
         assert "You can only book a maximum of 8 places" in data
     
+    def test_booking_more_than_12_places_should_fail(self, client):
+        response = client.post("/purchasePlaces", data={"competition": "competition_test1",
+                                                        "club": "club_test1",
+                                                        "places": 15})
+        assert response.status_code == 200
+        data = response.data.decode()
+        assert "You can only book a maximum of 12 places" in data
+
+    
     def test_booking_max_allowed_places_respected_in_ui(self, client):
 
         # with client.session_transaction() as session:
         #     session["logged_club"] = {'name': 'club_test1'}
 
-        # Competition : 50 places  | Club : 20 points | Booked : 0
+        # Competition : 50 places / Club : 20 points
         response = client.get('/book/competition_test1/club_test1')
         assert response.status_code == 200
         data = response.data.decode()
-        assert 'name="places" value="1" min="1" max="20"' in data
+        assert 'name="places" value="1" min="1" max="12"' in data
 
-        # Competition : 15 places  | Club : 20 points | Booked : 0
+        # Competition : 15 places / Club : 20 points
         response = client.get('/book/competition_test2/club_test1')
         data = response.data.decode()
-        assert 'name="places" value="1" min="1" max="15"' in data
+        assert 'name="places" value="1" min="1" max="12"' in data
 
-        # Competition : 6 places  | Club : 20 points | Booked : 0
+        # Competition : 6 places / Club : 20 points
         response = client.get('/book/competition_test3/club_test1')
         data = response.data.decode()
         assert 'name="places" value="1" min="1" max="6"' in data
