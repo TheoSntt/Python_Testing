d2748c55813d2bbf0351e6fd013087768421fd62
Théo <sinatti.theo@gmail.com>
2023-06-26 17:43:31 +0200

    Update the tests so they expects the clubs points to be updated. They now fail.

--- tests/unit/test_booking.py
+++ tests/unit/test_booking.py
@@ -23,7 +23,7 @@ class TestBookingClass:
         assert "Great-booking complete!" in data
     """
     CLUB 1 Points : 30
-    CLUB 2 Points : 8 (because we don't deduct the points yet)
+    CLUB 2 Points : 2
     CLUB 3 Points : 0
     _______
 
@@ -32,15 +32,23 @@ class TestBookingClass:
     COMP 3 Places : 6
     """
     def test_booking_more_places_than_available_points_should_fail(self, client):
+        # Testing with club 3
         response = client.post("/purchasePlaces", data={"competition": "competition_test1",
                                                         "club": "club_test3",
                                                         "places": 10})
         assert response.status_code == 200
         data = response.data.decode()
         assert "You can only book a maximum of 0 places" in data
+        # Testing with club 2 who previously had 8 points but now should only have 2 left
+        response = client.post("/purchasePlaces", data={"competition": "competition_test1",
+                                                        "club": "club_test2",
+                                                        "places": 4})
+        assert response.status_code == 200
+        data = response.data.decode()
+        assert "You can only book a maximum of 2 places" in data
     """
     CLUB 1 Points : 30
-    CLUB 2 Points : 8 (because we don't deduct the points yet)
+    CLUB 2 Points : 2
     CLUB 3 Points : 0
     _______
 
@@ -57,17 +65,10 @@ class TestBookingClass:
         assert "You can only book a maximum of 12 places" in data
     
     def test_booking_more_than_12_places_in_two_iterations_should_fail(self, client):
-        # Trying to book 8 more places by club 2 who already booked 6
-        response = client.post("/purchasePlaces", data={"competition": "competition_test1",
-                                                        "club": "club_test2",
-                                                        "places": 8})
-        assert response.status_code == 200
-        data = response.data.decode()
-        assert "You can only book a maximum of 6 places" in data
         # Now trying to buy 12 places twice with club 1. First one should work
         response = client.post("/purchasePlaces", data={"competition": "competition_test1",
                                                         "club": "club_test1",
-                                                        "places": 12})
+                                                        "places": 10})
         assert response.status_code == 200
         data = response.data.decode()
         assert "Great-booking complete!" in data
@@ -77,17 +78,17 @@ class TestBookingClass:
                                                         "places": 12})
         assert response.status_code == 200
         data = response.data.decode()
-        assert "You can only book a maximum of 0 places" in data
+        assert "You can only book a maximum of 2 places" in data
 
     
     def test_booking_max_allowed_places_respected_in_ui(self, client):
         """
-        CLUB 1 Points : 30 (because we don't deduct the points yet)
-        CLUB 2 Points : 8 (because we don't deduct the points yet)
+        CLUB 1 Points : 20
+        CLUB 2 Points : 2
         CLUB 3 Points : 0
         _______
 
-        COMP 1 Places : 32 (6 booked by club 2, 12 by club 1)
+        COMP 1 Places : 34 (6 booked by club 2, 10 by club 1)
         COMP 2 Places : 15
         COMP 3 Places : 6
         """
