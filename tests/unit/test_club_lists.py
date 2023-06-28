71ccf015b27297e128da116fa9a264bab93c86e7
Théo <sinatti.theo@gmail.com>
2023-06-28 10:38:18 +0200

    Add a test on expected content of clubs list display

--- tests/unit/test_club_lists.py
+++ tests/unit/test_club_lists.py
@@ -5,3 +5,19 @@ class TestClubs:
         assert response.status_code == 200
         data = response.data.decode()
         assert "List of clubs || GUDLFT" in data
+    
+    def test_list_display(self, client):
+        response = client.get('/showClubs')
+        assert response.status_code == 200
+        data = response.data.decode()
+        assert 'club_test1' in data
+        assert 'test1@email.fr' in data
+        assert 'Points: 30' in data
+
+        assert 'club_test2' in data
+        assert 'test2@email.fr' in data
+        assert 'Points: 8' in data
+
+        assert 'club_test2' in data
+        assert 'test3@email.fr' in data
+        assert 'Points: 0' in data
